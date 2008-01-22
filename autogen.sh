eb8ccc4f9bc94491afbdbe3a8668c99732303ea2
Johann George (QLogic) <johann@hosting.openfabrics.org>
2008-01-22 13:03:43 -0800

    Testing git

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@ set -x
 for f in AUTHORS NEWS README ChangeLog; do
     [ -e "$f" ] || touch "$f"
 done
-aclocal
-automake --foreign --add-missing --copy
-autoconf
+aclocal &&
+automake --foreign --add-missing --copy &&
+autoconf &&
+./configure
