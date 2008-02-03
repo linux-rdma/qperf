7b750d864bf94b4f9fdfb61cf7cbb8bf2a81c5af
Johann George (QLogic) <johann@hosting.openfabrics.org>
2008-02-03 14:10:07 -0800

    More changes to support OFED scripts

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,6 @@ set -x
 for f in NEWS README ChangeLog; do
     [ -e "$f" ] || touch "$f"
 done
-aclocal
-automake --foreign --add-missing --copy
-autoconf
-./configure
+aclocal &&
+    automake --foreign --add-missing --copy &&
+    autoconf
